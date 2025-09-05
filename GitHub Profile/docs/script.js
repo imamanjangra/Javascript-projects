9d9a295bd388d0e22b592826dba0d3aaeeb66393
imamanjangra <imamanjangra@gmail.com>
2025-09-05 23:52:07 +0530

    done

--- GitHub Profile/docs/script.js
+++ GitHub Profile/docs/script.js
@@ -1,5 +1,8 @@
 
   //  const repoResponse = await fetch(`https://api.github.com/users/${username}/repos?per_page=100`);
+
+// const { jsx } = require("react/jsx-runtime");
+
      
 
 let submit_btn = document.querySelector(".submit_btn");
@@ -10,6 +13,7 @@ let P_name = document.querySelector("#name");
 let bio = document.querySelector("#bio");
 let followers = document.querySelector("#followers");
 let following = document.querySelector("#following");
+let repos = document.querySelector("#repos");
 
 
 submit_btn.addEventListener('click' , (e)=>{
@@ -26,34 +30,78 @@ function input_username(e){
 
 function run_api(id){
   let profile_link = `https://api.github.com/users/${id}`;
-  let repo_link = `https://api.github.com/users/${id}/repos?per_page=100`;
+
   let getdata1 = async ()=>{
   let request = await fetch(profile_link);
   let data = await request.json();
   console.log(data);
-  data_feach(data)
-
-  // let getdata2 = async ()=>{
-  //   let request2 = await fetch(repo_link);
-  //   let data2 = await request2.json();
-  //   console.log(data2);
-  //   data_feach(data2);
-
-  // }
-  // getdata2();
+  data_feach(data);
 }
   
-
 getdata1();
-// getdata2();
 }
 
-function data_feach(data){
+function data_feach (data)
+{
   console.log(data.name)
   P_name.innerText = `${data.name}`
   avatar.src = `${data.avatar_url}`;
   bio.innerText = `${data.bio}`;
   followers.innerText = `${data.followers}`
   following.innerText = `${data.following}`
+  let repo_url = data.repos_url;
+  let lan_url = data.received_events_url;
+  console.log(lan_url);
+   
+  let get_url = async ()=>{
+    let request_repo = await fetch(repo_url);
+    let repo_data = await request_repo.json();
+    console.log(repo_data);
+    repo_data_use(repo_data)
+  }
+  get_url();
+  
+  let stars_url = async ()=>{
+    let request_star = await fetch(lan_url);
+    let repo_star = await request_star.json();
+    console.log(repo_star);
+    repo_data_use(repo_star)
+  }
+  stars_url();
 
+}
+
+function repo_data_use (repo_data){
+  let length = repo_data.length;
+ 
+
+  for(let i = 0; i<length; i++){
+     
+    repo_data.sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at));
+    let top_three = repo_data.slice(0, 3);
+    repos.innerHTML = "";
+
+  top_three.forEach(repo => {
+      let li = document.createElement("li");
+   
+      let link = document.createElement("a");
+     link.innerText = repo.name;
+     link.target = "_blank";
+     link.href = repo.html_url;
+
+      let p = document.createElement("span");
+      p.innerText = `⭐ ${repo.stargazers_count} | 🍴 ${repo.forks_count} | ${repo.language}`;
+      li.style.display = "flex";
+      li.style.justifyContent = "space-between";
+      li.style.alignItems = "center";
+      li.appendChild(link);
+      
+      li.appendChild(p);
+
+      repos.appendChild(li);
+      
+});
+
+
+  }
 }
