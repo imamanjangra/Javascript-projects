e97e12ab7b9492a6d82bd8b494f97af3c74f9c62
tarun <tarunjangra1903@gmail.com>
2025-09-06 15:04:01 +0530

    ui changed

--- GitHub Profile/docs/script.js
+++ GitHub Profile/docs/script.js
@@ -1,10 +1,3 @@
-
-  //  const repoResponse = await fetch(`https://api.github.com/users/${username}/repos?per_page=100`);
-
-// const { jsx } = require("react/jsx-runtime");
-
-     
-
 let submit_btn = document.querySelector(".submit_btn");
 let username = document.querySelector("#username");
 let profile = document.querySelector("#profile-container");
@@ -14,110 +7,97 @@ let bio = document.querySelector("#bio");
 let followers = document.querySelector("#followers");
 let following = document.querySelector("#following");
 let repos = document.querySelector("#repos");
+let profileSkeleton = document.querySelector("#profile-skeleton");
+let profileData = document.querySelector("#profile-data");
 
-
-submit_btn.addEventListener('click' , (e)=>{
+submit_btn.addEventListener("click", (e) => {
   input_username(e);
-})
+});
 
-username.addEventListener('keydown' , (e)=>{
-
-  if(e.key == "Entre"){
+username.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
     input_username(e);
   }
-})
+});
 
-function input_username(e){
+function input_username(e) {
   e.preventDefault();
-  let id = username.value.replace(/\s+/g , "");
-  console.log(id);
-  if(id == ""){
-    alert("Entre your GIthub username!");
+  let id = username.value.replace(/\s+/g, "");
+  if (id === "") {
+    alert("Enter your GitHub username!");
     return;
-  }
-  else{
-
+  } else {
     run_api(id);
   }
 }
 
-function run_api(id){
+function run_api(id) {
   let profile_link = `https://api.github.com/users/${id}`;
 
-  let getdata1 = async ()=>{
-  let request = await fetch(profile_link);
-  let data = await request.json();
-
-  console.log(data);
-
-  if(data.status == 404){
-    alert("Gihtub username is wrong!");
-    return;
-  }
-  else{
-
-    data_feach(data);
+  // show container and skeleton
   profile.classList.remove("hidden");
+  profileSkeleton.classList.remove("hidden");
+  profileData.classList.add("hidden");
 
-  }
-}
-  
-getdata1();
+  let getdata1 = async () => {
+    let request = await fetch(profile_link);
+    let data = await request.json();
+
+    if (data.message === "Not Found") {
+      alert("GitHub username is wrong!");
+      profile.classList.add("hidden");
+      return;
+    } else {
+      data_fetch(data);
+    }
+  };
+
+  getdata1();
 }
 
-function data_feach (data)
-{
-  
-  console.log(data.name)
-  P_name.innerText = `${data.name}`
-  avatar.src = `${data.avatar_url}`;
-  bio.innerText = `${data.bio}`;
-  followers.innerText = `${data.followers}`
-  following.innerText = `${data.following}`
+function data_fetch(data) {
+  P_name.innerText = data.name || data.login;
+  avatar.src = data.avatar_url;
+  bio.innerText = data.bio || "No bio available";
+  followers.innerText = data.followers;
+  following.innerText = data.following;
+
   let repo_url = data.repos_url;
 
-  let get_url = async ()=>{
+  let get_url = async () => {
     let request_repo = await fetch(repo_url);
     let repo_data = await request_repo.json();
-    console.log(repo_data);
-    repo_data_use(repo_data)
-  }
+    repo_data_use(repo_data);
+
+    // hide skeleton, show real profile
+    profileSkeleton.classList.add("hidden");
+    profileData.classList.remove("hidden");
+  };
   get_url();
-  
-  
 }
 
-function repo_data_use (repo_data){
-  let length = repo_data.length;
- 
+function repo_data_use(repo_data) {
+  repos.innerHTML = "";
+  repo_data.sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at));
+  let top_three = repo_data.slice(0, 3);
 
-  for(let i = 0; i<length; i++){
-     
-    repo_data.sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at));
-    let top_three = repo_data.slice(0, 3);
-    repos.innerHTML = "";
+  top_three.forEach((repo) => {
+    let li = document.createElement("li");
+    li.className =
+      "flex justify-between items-center bg-[#0d1117] px-4 py-2 rounded-lg border border-green-500/20 hover:border-green-400 transition";
 
-  top_three.forEach(repo => {
-      let li = document.createElement("li");
-   
-      let link = document.createElement("a");
-     link.innerText = repo.name;
-     link.target = "_blank";
-     link.href = repo.html_url;
+    let link = document.createElement("a");
+    link.innerText = repo.name;
+    link.target = "_blank";
+    link.href = repo.html_url;
+    link.className = "text-green-400 hover:text-green-300 font-medium";
 
-      let p = document.createElement("span");
-      p.innerText = `⭐ ${repo.stargazers_count} | 🍴 ${repo.forks_count} | ${repo.language}`;
-      li.style.display = "flex";
-      li.style.justifyContent = "space-between";
-      li.style.alignItems = "center";
-      li.appendChild(link);
-      
-      li.appendChild(p);
+    let p = document.createElement("span");
+    p.innerText = `⭐ ${repo.stargazers_count} | 🍴 ${repo.forks_count} | ${repo.language || "N/A"}`;
+    p.className = "text-green-500 text-xs ml-3";
 
-      repos.appendChild(li);
-      
-});
-
-
-  }
+    li.appendChild(link);
+    li.appendChild(p);
+    repos.appendChild(li);
+  });
 }
